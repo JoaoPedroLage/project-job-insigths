3b8326ba7cdeff90e043540f30778d6fdb0ad296
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-07-01 14:38:24 -0300

    start: [Joao Pedro Lage] Project - Job Insights (Python)

--- src/jobs.py
+++ src/jobs.py
@@ -1,6 +1,16 @@
 from functools import lru_cache
 
 
+# nome,cidade,telefone
+# Ana,Curitiba,1111111
+# Bernardo,Santos,999999
+
+#   [
+#     {"nome": "Ana", "cidade": "Curitiba", "telefone": "1111111"},
+#     {"nome": "Bernardo", "cidade": "Santos", "telefone": "999999"}
+#   ]
+
+
 @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
